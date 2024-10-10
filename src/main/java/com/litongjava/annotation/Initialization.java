d903efa0f83cc49c00fa7642acdb6a05a7dbe33a
litongjava <litongjava@qq.com>
2024-10-09 15:41:48 -1000

    rename to Initialization

--- src/main/java/com/litongjava/annotation/Initialization.java
+++ src/main/java/com/litongjava/annotation/Initialization.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface AInitialization {
+public @interface Initialization {
 
   String value() default "";
   /**
