d341a0bf4228c290504d4480f9d428ccc7d5ab71
litongjava <litongjava@qq.com>
2024-12-27 20:06:00 -1000

    update BootConfiguration

--- src/main/java/com/litongjava/context/BootConfiguration.java
+++ src/main/java/com/litongjava/context/BootConfiguration.java
@@ -1,7 +1,5 @@
 package com.litongjava.context;
 
-import java.io.IOException;
-
 public interface BootConfiguration {
-  public void config() throws IOException;
+  public void config() throws Exception;
 }
