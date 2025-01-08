988d6f5e2297e376039c8af8865d24f816e018f5
litongjava <litongjava@qq.com>
2025-01-07 19:12:19 -1000

    update HookCan

--- src/main/java/com/litongjava/hook/HookCan.java
+++ src/main/java/com/litongjava/hook/HookCan.java
@@ -1,13 +1,16 @@
 package com.litongjava.hook;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
+@Slf4j
 public class HookCan {
 
   private static HookCan me = new HookCan();
@@ -34,5 +37,19 @@ public class HookCan {
 
   public void clear() {
     destroyMethods.clear();
+  }
+  
+  public void stop() {
+    Iterator<Runnable> iterator = destroyMethods.iterator();
+    while (iterator.hasNext()) {
+      Runnable runnable = iterator.next();
+      iterator.remove();
+      try {
+        runnable.run();
+      } catch (Exception e) {
+        log.error("error occured while :{}", runnable);
+      }
+    }
+    me = new HookCan();
   }
 }
