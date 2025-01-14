ad1416ff92cd78724a5d60930e9e1c38bc48d13b
litongjava <litongjava@qq.com>
2025-01-13 23:12:53 -1000

    update ok result

--- src/main/java/com/litongjava/db/OkResult.java
+++ src/main/java/com/litongjava/db/OkResult.java
@@ -12,7 +12,7 @@ import lombok.experimental.Accessors;
 public class OkResult<T> implements java.io.Serializable {
   private static final long serialVersionUID = -4806033278155820866L;
   private T v;
-  private boolean ok;
+  private boolean ok = true;
   private Exception e;
 
   public OkResult(boolean b) {
@@ -50,12 +50,31 @@ public class OkResult<T> implements java.io.Serializable {
   public static OkResult exception(Exception e) {
     return new OkResult(false, e);
   }
-  
+
   public static <T> OkResult<T> fail(T value) {
     return new OkResult<T>(false, value);
   }
 
   public static <T> OkResult<T> ok(T value) {
     return new OkResult<T>(true, value);
+  }
+
+  public OkResult<T> notOk() {
+    this.ok = false;
+    return this;
+  }
+
+  public OkResult<T> notOk(Exception e) {
+    this.ok = false;
+    this.e = e;
+    return this;
+  }
+
+  public boolean isNotOk() {
+    return this.ok == false;
+  }
+
+  public T getResult() {
+    return v;
   }
 }
