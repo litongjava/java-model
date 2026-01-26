99248eaa52e8737ff0c9b2ee467e65d59fcda3aa
litongjava <litongjava001@gmail.com>
2026-01-26 23:36:37 +0800

    add DbJsonObject

---
+++ src/main/java/com/litongjava/db/DbJsonObject.java
@@ -0,0 +1,24 @@
+package com.litongjava.db;
+
+public class DbJsonObject {
+  public String value;
+
+  public DbJsonObject() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public DbJsonObject(String value) {
+    super();
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+}
